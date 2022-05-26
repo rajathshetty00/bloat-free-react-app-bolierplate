f09c9342a805e488a9671b5a933eedb862ae6cdd
rajathshetty00 <rajathshetty00@gmail.com>
2022-05-26 14:44:48 +0530

    initial setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.scss';
+
+
+ReactDOM.render(
+    <h1>Hello from index.js</h1>,
+    document.querySelector(
+      '#fromjs'
+    )
+  );
